927a83f1485077cfb7a035514ac1f60885bf5713
Micaela Ocampo <ocampomica1809@gmail.com>
2023-01-17 10:53:11 -0300

    add details

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -14,7 +14,7 @@ const Intro = () => {
                             <div className="i-title-item">Guarderia felina</div>
                             <div className="i-title-item">Cuidadores a domicilio</div>
                             <div className="i-title-item">Adopciones</div>
-                            <div className="i-title-item">Publicidad</div>                        
+                            <div className="i-title-item">Publicidad para tus servicios</div>                        
                         </div>
                     </div>
                     <p className="i-desc">
