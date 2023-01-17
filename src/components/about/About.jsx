569263ed332d3e643689f0d5cd245a73880a95aa
Micaela Ocampo <ocampomica1809@gmail.com>
2023-01-17 12:15:28 -0300

    about img change

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,6 +1,6 @@
 import "./about.css";
 import skills from "../../img/skill.png";
-import aboutme from "../../img/aboutme.png";
+import aboutme from "../../img/aboutme.jpg";
 
 const About = () => {
     return (
